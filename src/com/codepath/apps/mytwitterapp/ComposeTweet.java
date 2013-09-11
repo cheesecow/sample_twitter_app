d65801313a8935d224151c172731bd668723c320
Eric N <eric@change.org>
2013-09-10 20:29:49 -0700

    initial commit

---
+++ src/com/codepath/apps/mytwitterapp/ComposeTweet.java
@@ -0,0 +1,52 @@
+package com.codepath.apps.mytwitterapp;
+
+import org.json.JSONArray;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class ComposeTweet extends Activity {
+	EditText etNewTweet;
+	Button btnTweet;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compose_tweet);
+		setupViews();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.compose_tweet, menu);
+		return true;
+	}
+	
+	public void composeTweet(View v){
+		String message = etNewTweet.getText().toString();
+		RequestParams params = new RequestParams("status", message);
+		
+		MyTwitterApp.getRestClient().postHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				setResult(RESULT_OK);
+				finish(); 
+			} 
+		}, params);
+	}
+	
+	public void setupViews(){
+		etNewTweet = (EditText) findViewById(R.id.etNewTweet);
+		btnTweet = (Button) findViewById(R.id.btnTweet);
+	}
+
+}
---
+++ src/com/codepath/apps/mytwitterapp/TimelineActivity.java
@@ -0,0 +1,62 @@
+package com.codepath.apps.mytwitterapp;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.codepath.apps.mytwitterapp.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+public class TimelineActivity extends Activity {
+
+	public void composeTweet(MenuItem mi) {
+		Intent i = new Intent(getApplicationContext(), ComposeTweet.class);
+		startActivity(i);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  if (resultCode == RESULT_OK ) {
+			refreshTimeline();
+	  }
+	} 
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+		
+		
+		
+		refreshTimeline();
+	}
+
+	public void refreshTimeline() {
+		MyTwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				ArrayList<Tweet> tweets =  Tweet.fromJson(jsonTweets);
+				ListView lvTweets = (ListView)findViewById(R.id.lvTweets);
+				TweetsAdapter adapter = new TweetsAdapter(getBaseContext(), tweets);
+				
+				lvTweets.setAdapter(adapter);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+
+}
